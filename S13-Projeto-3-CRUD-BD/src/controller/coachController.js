ab6fe0467113f9b99852eda67072344b5532bb65
Carol Alves <prof.carolalves90@gmail.com>
2022-06-29 22:03:19 -0300

    :lock: protegendo codigo de criar e de encontrar coach

--- S13-Projeto-3-CRUD-BD/src/controller/coachController.js
+++ S13-Projeto-3-CRUD-BD/src/controller/coachController.js
@@ -1,36 +1,37 @@
-
-// post -> criar um treinador
-// get -> visualizar todos os treinadores
-// get -> visualizar um treinador por id
-// patch -> 
-// delete ->
-
-/**
- * 
- * const { name, age, team } = req.body
- * const age = req.body.age
- * const name = req.body.name
- * const team = req.body.team
- */
 const CoachModel = require('../models/coachModel')
 const SECRET = process.env.SECRET
 const jwt = require('jsonwebtoken')
 
 const createCoach = async (req, res) => {
-   try {
-      const { name, team, region, age, gender } = req.body
+    try {
+      const authHeader = req.get('authorization')
 
-      const newCoach = new CoachModel({
-        name, team, region, age, gender
+      if(!authHeader){
+        return res.status(401).send('Cade o header?')
+      }
+
+      const token = authHeader.split(' ')[1]
+
+      await jwt.verify(token, SECRET, async function (erro) {
+        if (erro) {
+          return res.status(403).send('Nope')
+        }
+      
+        const { name, team, region, age, gender } = req.body
+
+        const newCoach = new CoachModel({
+          name, team, region, age, gender
+        })
+  
+        const savedCoach = await newCoach.save()
+  
+        res.status(201).json(savedCoach)
+      
       })
-
-      const savedCoach = await newCoach.save()
-
-      res.status(201).json(savedCoach)
-   } catch (error) {
+    } catch (error) {
      console.error(error)
      res.status(500).json({ message: error.message })
-   }
+    }
 }
 
 const findAllCoaches = async (req, res) => {
@@ -60,8 +61,19 @@ const findAllCoaches = async (req, res) => {
 
 const findCoachById = async (req, res) => {
    try {
-     const findCoach = await CoachModel.findById(req.params.id)
-     res.status(200).json(findCoach)
+     const authHeader = req.get('Authorization')
+     if(!authHeader){
+      return res.status(401).send('Cade o authorization?')
+     }
+     const token = authHeader.split(' ')[1]
+
+     await jwt.verify(token, SECRET, async function (erro){
+      if(erro){
+        return res.status(403).send('Não vai rolar')
+      }
+      const findCoach = await CoachModel.findById(req.params.id)
+      res.status(200).json(findCoach)
+     })
    } catch (error) {
      console.error(error)
      res.status(500).json({ message: error.message })
