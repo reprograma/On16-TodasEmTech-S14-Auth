b7015d3d649afc02589b1013a4a893014afea172
Louise <louicosta.silva@gmail.com>
2022-06-29 22:09:06 -0300

    Adiciona pasta de database

---
+++ src/database/mongooseConnect.js
@@ -0,0 +1,20 @@
+const DATABASE_URI = "mongodb+srv://apimusica:1234abcd@cluster0.mt9gn.mongodb.net/projeto_db"
+
+const mongoose = require('mongoose')
+
+const connect = async() => {
+   try {
+     await mongoose.connect(DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+     })
+
+     console.log('banco conectado! ')
+   } catch (error) {
+    console.error(error)
+   }
+}
+
+module.exports = {
+  connect
+}
