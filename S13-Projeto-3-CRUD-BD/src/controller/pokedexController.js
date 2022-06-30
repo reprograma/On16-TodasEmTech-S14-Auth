cea6f7058c45c764246ee6632a07b4929598f7c6
Carol Alves <prof.carolalves90@gmail.com>
2022-06-30 15:30:27 -0300

    :lock: protegendo codigo de criar pokedex

--- S13-Projeto-3-CRUD-BD/src/controller/pokedexController.js
+++ S13-Projeto-3-CRUD-BD/src/controller/pokedexController.js
@@ -1,8 +1,22 @@
 const PokedexModel = require('../models/pokedexModel')
 const CoachModel = require('../models/coachModel')
+const SECRET = process.env.SECRET
+const jwt = require('jsonwebtoken')
 
 const createPokemon = async (req, res) => {
    try {
+     const authHeader = req.get('authorization')
+
+     if(!authHeader){
+      return res.status(401).send('Cade o authorization?')
+     }
+     const token = authHeader.split(' ')[1]
+     
+     await jwt.verify(token, SECRET, async function(erro){
+      if(erro){
+        return res.status(403).send('Não vai rolar')
+      }
+
      const { coachId, name, type, abilities, description } = req.body //  <-
      
      if (!coachId) {
@@ -15,7 +29,6 @@ const createPokemon = async (req, res) => {
       return res.status(404).json({ message: 'Treinador não foi encontrado'})
      }
 
-     // -->
      const newPokemon = new PokedexModel({
       coach: coachId,
       name, type, abilities, description
@@ -24,7 +37,7 @@ const createPokemon = async (req, res) => {
      const savedPokemon = await newPokemon.save()
 
      res.status(200).json(savedPokemon)
-
+    })
    } catch (error) {
     console.error(error)
     res.status(500).json({ message: error.message })
