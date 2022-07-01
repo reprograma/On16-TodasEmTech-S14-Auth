cb215f2ee851fcc2d7abba408aa1869c946a56cb
caroliinaasilva <carolinadesousa84@yahoo.com>
2022-07-01 13:34:15 -0300

    Revisão e Entrega

--- S13-Projeto-3-CRUD-BD/server.js
+++ S13-Projeto-3-CRUD-BD/server.js
@@ -1,5 +1,3 @@
-// 1. importa o app(api)
-// 2. definir uma porta e ouvi-la
 const app = require('./src/app')
 const PORT = 8000
 app.listen(PORT, () => console.log(`fé no pai que agora vai, porta: 8000`))
