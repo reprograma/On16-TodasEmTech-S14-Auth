aa25e827be236c8a5c1e6b783cadcd12c3f90f81
nathalia <nacaldas@hotmail.com>
2022-07-01 16:32:23 -0300

    Atualiza PORT

--- S13-Projeto-3-CRUD-BD/server.js
+++ S13-Projeto-3-CRUD-BD/server.js
@@ -1,5 +1,4 @@
-// 1. importa o app(api)
-// 2. definir uma porta e ouvi-la
 const app = require('./src/app')
+const DB_PORT = process.env.DB_PORT 
 
-app.listen(8000, () => console.log(`fé no pai que agora vai, porta: 8000`))
+app.listen(DB_PORT, () => console.log(`fé no pai que agora vai, porta: ${DB_PORT}`))
