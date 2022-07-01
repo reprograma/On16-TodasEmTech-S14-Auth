e7feed95f7a1c35a0323bfbb947caea9b65e83b8
DayseDev <dayse_santos22@hotmail.com>
2022-07-01 17:36:07 -0300

    Projeto - Semana 14

--- S13-Projeto-3-CRUD-BD/server.js
+++ S13-Projeto-3-CRUD-BD/server.js
@@ -3,3 +3,7 @@
 const app = require('./src/app')
 
 app.listen(8000, () => console.log(`fé no pai que agora vai, porta: 8000`))
+
+const PORT = process.env.PORT
+
+app.listen(PORT, () => console.log(`fé no pai que agora vai, porta: ${PORT}`))
