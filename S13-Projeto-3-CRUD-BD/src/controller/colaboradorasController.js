117c5e3dd0b9c503f0abd5493c3cd81cf8a13020
Beatriz Andrade <beatrizrandrade@gmail.com>
2022-07-01 19:50:30 -0300

    Lógica rotas de login/colab

---
+++ S13-Projeto-3-CRUD-BD/src/controller/colaboradorasController.js
@@ -0,0 +1,95 @@
+const colaboradoras = require('../models/colaboradorasModel')
+const Colaboradoras = require('../models/colaboradorasModel')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const SECRET = process.env.SECRET
+
+const create = (req, res) => {
+    const senhaComHash = bcrypt.hashSync(req.body.senha, 10)
+
+    console.log('minha senha sem hash: ', req.body.senha)
+    console.log('minha senha com hash: ', senhaComHash)
+
+    req.body.senha = senhaComHash
+
+    console.log('meu body.senha atualizado: ', req.body.senha)
+
+    const colaboradora = new Colaboradoras(req.body)
+    colaboradora.save(function (err) {
+        if (err) {
+            res.status(500).send({
+                message: err.message
+            })
+        }
+        res.status(201).send(colaboradora)
+    })
+}
+
+const getAll = (req, res) => {
+    Colaboradoras.find(function (err, colaboradoras) {
+        if (err) {
+            res.status(500).send({
+                message: err.message
+            })
+        }
+        res.status(200).send(colaboradoras)
+    })
+}
+
+const deleteById = async (req, res) => {
+    try {
+        const {
+            id
+        } = req.params
+        await Colaboradoras.findByIdAndDelete(id)
+        const message = `Colaboradora com o id ${id} foi deletada com sucesso!!!`
+        res.status(200).json({
+            message
+        })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+/*
+req.body.email
+req.body.senha
+*/
+
+
+const login = (req, res) => {
+    Colaboradoras.findOne({
+        email: req.body.email
+    }, function (error, colaboradora) {
+        if (error) {
+            return res.status(500).send({
+                message: 'Deu errado'
+            })
+        }
+        if (!colaboradora) {
+            return res.status(404).send("não existe colaboradora com este email!")
+        }
+
+        const senhaValida = bcrypt.compareSync(req.body.senha, colaboradora.senha)
+
+        if (!senhaValida) {
+            return res.status(403).send("que senha é essa?")
+        }
+
+        const token = jwt.sign({
+            email: req.body.email
+        }, SECRET)
+        return res.status(200).send(token)
+    })
+}
+
+
+module.exports = {
+    create,
+    getAll,
+    deleteById,
+    login
+}
