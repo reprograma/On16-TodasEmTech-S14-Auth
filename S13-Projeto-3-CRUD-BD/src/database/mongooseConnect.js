2b478855984d6f14303ba1ff4787dad98ba2b0c4
Maysafig <maysafig@gmail.com>
2022-07-02 02:08:12 -0300

    Atividade semana14

--- S13-Projeto-3-CRUD-BD/src/database/mongooseConnect.js
+++ S13-Projeto-3-CRUD-BD/src/database/mongooseConnect.js
@@ -1,4 +1,4 @@
-const DATABASE_URI = "mongodb+srv://apimusica:1234abcd@cluster0.mt9gn.mongodb.net/projeto_db"
+const DATABASE_URI = process.env.DATABASE_URI
 
 const mongoose = require('mongoose')
 
