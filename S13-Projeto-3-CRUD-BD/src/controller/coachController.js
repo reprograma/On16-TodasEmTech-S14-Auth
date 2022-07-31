8711bf97dab484e0efc311c6d38dbe0bff0d3c3c
Lia Laurindo <liamaralaurindo@gmail.com>
2022-07-30 22:48:02 -0300

    atualização de projeto com autenticação de rotas

--- S13-Projeto-3-CRUD-BD/src/controller/coachController.js
+++ S13-Projeto-3-CRUD-BD/src/controller/coachController.js
@@ -1,10 +1,4 @@
 
-// post -> criar um treinador
-// get -> visualizar todos os treinadores
-// get -> visualizar um treinador por id
-// patch -> 
-// delete ->
-
 /**
  * 
  * const { name, age, team } = req.body
@@ -12,70 +6,138 @@
  * const name = req.body.name
  * const team = req.body.team
  */
-const CoachModel = require('../models/coachModel')
-const createCoach = async (req, res) => {
-   try {
-      const { name, team, region, age, gender } = req.body
-
-      const newCoach = new CoachModel({
-        name, team, region, age, gender
-      })
-
-      const savedCoach = await newCoach.save()
-
-      res.status(201).json(savedCoach)
-   } catch (error) {
-     console.error(error)
-     res.status(500).json({ message: error.message })
-   }
-}
-
-const findAllCoaches = async (req, res) => {
-  try {
-    const allCoaches = await CoachModel.find()
-    res.status(200).json(allCoaches)
-  } catch(error) {
-    console.error(error)
-    res.status(500).json({ message: error.message})
-  }
-}
-
-const findCoachById = async (req, res) => {
-   try {
-     const findCoach = await CoachModel.findById(req.params.id)
-     res.status(200).json(findCoach)
-   } catch (error) {
-     console.error(error)
-     res.status(500).json({ message: error.message })
-   }
-}
-
-const updateCoach = async (req, res) => {
-  try {
-    const { name, age, region, team, gender } = req.body
-    const updatedCoach = await CoachModel
-    .findByIdAndUpdate(req.params.id, {
-      name, age, region, team, gender
+ const CoachModel = require('../models/coachModel')
+ const jwt = require('jsonwebtoken')
+ const SECRET = process.env.SECRET
+ 
+ 
+ const createCoach = async (req, res) => {
+    try {
+     const authHeader =req.get('authorization')
+     if(!authHeader){
+       return res.status(401).send("Cade authorization?")
+     }
+  
+     const token = authHeader.split(' ')[1]
+     await jwt.verify(token,SECRET,async function(erro){
+       if(erro){
+         return res.status(403).send("não vai rolar")
+      }
+ 
+       const { name, team, region, age, gender } = req.body
+ 
+       const newCoach = new CoachModel({
+         name, team, region, age, gender
+       })
+ 
+       const savedCoach = await newCoach.save()
+ 
+       res.status(201).json(savedCoach)
     })
-    res.status(200).json(updatedCoach)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: error.message })
-  }
-}
-
-const deleteCoach = async (req, res) => {
+ }catch (error) {
+   console.error(error)
+   res.status(500).json({ message: error.message })
+ }
+ }
+ 
+ const findAllCoaches = async (req, res) => {
    try {
-       const { id } = req.params
-       await CoachModel.findByIdAndDelete(id)
-       const message = `O treinador com o ${id} foi deletado com sucesso!`
-      res.status(200).json({ message })
-   } catch (error) {
+     const authHeader =req.get('authorization')
+     if(!authHeader){
+       return res.status(401).send("Cade authorization?")
+     }
+ 
+     const token = authHeader.split(' ')[1]
+     await jwt.verify(token,SECRET,async function(erro){
+       if(erro){ 
+         return res.status(403).send("não vai rolar")
+      }
+       const allCoaches = await CoachModel.find()
+       res.status(200).json(allCoaches)
+     })
+ 
+     
+   } catch(error) {
      console.error(error)
-     res.status(500).json({ message: error.message })
+     res.status(500).json({ message: error.message})
    }
-}
-
-module.exports =  {
-  createCoach, findAllCoaches, updateCoach, deleteCoach, findCoachById
-}
+      
+ }
+ 
+ 
+ const findCoachById = async (req, res) => {
+    try {
+     const authHeader =req.get('authorization')
+     if(!authHeader){
+       return res.status(401).send("Cade authorization?")
+     }
+  
+     const token = authHeader.split(' ')[1]
+     await jwt.verify(token,SECRET,async function(erro){
+       if(erro){
+         return res.status(403).send("não vai rolar")
+      }
+ 
+      const findCoach = await CoachModel.findById(req.params.id)
+      res.status(200).json(findCoach)
+    })
+ }catch (error) {
+   console.error(error)
+   res.status(500).json({ message: error.message })
+ }
+ }
+ 
+ 
+ const updateCoach = async (req, res) => {
+   try {
+     const authHeader =req.get('autorization')
+     if(!authHeader){
+       return res.status(401).send("Cade autorization?")
+     }
+  
+     const token = authHeader.split(' ')[1]
+     await jwt.verify(token,SECRET,async function(erro){
+       if(erro){
+         return res.status(403).send("não vai rolar")
+      }
+ 
+     const { name, age, region, team, gender } = req.body
+     const updatedCoach = await CoachModel
+     .findByIdAndUpdate(req.params.id, {
+       name, age, region, team, gender
+     })
+     res.status(200).json(updatedCoach)
+   })
+ }catch (error) {
+   console.error(error)
+   res.status(500).json({ message: error.message })
+ }
+ }
+ 
+ const deleteCoach = async (req, res) => {
+    try {
+     const authHeader =req.get('authorization')
+     if(!authHeader){
+       return res.status(401).send("Cade authorization?")
+     }
+  
+     const token = authHeader.split(' ')[1]
+     await jwt.verify(token,SECRET,async function(erro){
+       if(erro){
+         return res.status(403).send("não vai rolar")
+      }
+ 
+        const { id } = req.params
+        await CoachModel.findByIdAndDelete(id)
+        const message = `O treinador com o ${id} foi deletado com sucesso!`
+       res.status(200).json({ message })
+    })
+ }catch (error) {
+   console.error(error)
+   res.status(500).json({ message: error.message })
+ }
+ }
+ 
+ module.exports =  {
+   createCoach, findAllCoaches, updateCoach, deleteCoach, findCoachById
+ }
---
+++ S13-Projeto-3-CRUD-BD/src/controller/colaboradorasController.js
@@ -0,0 +1,70 @@
+const Colaboradoras= require('../models/colaboradorasModel')
+const bcrypt= require('bcrypt');
+const jwt= require('jsonwebtoken');
+const SECRET = process.env.SECRET
+
+
+
+const create = (req, res) => {
+    const senhaComHash = bcrypt.hashSync(req.body.senha, 10)
+    req.body.senha = senhaComHash
+  
+    const colaboradora = new Colaboradoras(req.body)
+    
+    colaboradora.save(function (err) {
+      if (err) {
+        res.status(500).send({message: err.message})
+      }
+      res.status(201).send(colaboradora)
+    })
+  }
+  
+  const getAll = (req, res) => {
+    Colaboradoras.find(function (err, colaboradoras) {
+      if (err) {
+        res.status(500).send({message: err.message})
+      }
+      res.status(201).send(colaboradoras)
+    })
+  }
+  
+  const deleteById = async (req, res) => {
+    try {
+  
+      const { id } = req.params
+      await Colaboradoras.findByIdAndDelete(id)
+      
+      res.status(200).json({message: `Colaboradora com id ${id} foi deletada`})
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({message: error.message})
+    }
+  }
+  
+  const login = (req, res) => {
+    Colaboradoras.findOne({email: req.body.email}, function (error, colaboradora) {
+      if (error) {
+        return res.status(500).send({message: 'deu ruim'})
+      }
+      if (!colaboradora) {
+        res.status(404).send(`Não existe colaboradora com esse email: ${req.body.email}`)
+      }
+      
+      const senhaValida = bcrypt.compareSync(req.body.senha, colaboradora.senha)
+  
+      if (!senhaValida) {
+        res.status(403).send('Senha inválida')
+      }
+      
+      const token = jwt.sign({email: req.body.email}, SECRET)
+       return res.status(200).send(token)
+    })
+    
+  }
+    
+  module.exports = {
+    create,
+    getAll,
+    deleteById,
+    login
+  }
